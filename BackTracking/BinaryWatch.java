ee532b92739fe91a822f4c574a4dafde811d75fc
Ran Cheng <neonasuka@gmail.com>
2018-12-25 16:49:13 -0800

    Update BinaryWatch.java

--- BackTracking/BinaryWatch.java
+++ BackTracking/BinaryWatch.java
@@ -26,3 +26,33 @@ class Solution {
 
 
 A:
+
+/*
+1.
+逆向思考：
+正向思考本题，可以得到小时h的binary为1的位数，进而得到分钟的binary为1的位数(即 num-h)。但是在知道h和m之后确定具体的小时和分钟数字时会
+遇到困难(可能的解法是用递归， 见解法2)。此时不妨逆向思考。
+逆向思考的话，我们直接go through所有可能的小时和分钟的数字(而不是为1的二进制位数)，进而构成一个完整的时间。判断此时间转换为二进制时1的
+位数是否等于num,如果是，即是我们想要的结果。
+
+但是，这种方法与backtracking无关。
+*/
+
+class Solution1 {
+    public List<String> readBinaryWatch(int num) {
+        List<String> times = new ArrayList<>();
+        for(int h=0; h<12; h++) {
+            for(int m=0; m<60; m++) {
+                if((Integer.bitCount(h) + Integer.bitCount(m)) == num) {
+                    times.add(String.format("%d:%02d",h,m));
+                }
+            }
+        }
+        return times;
+    }
+}
+
+/*
+2.
+
+*/
